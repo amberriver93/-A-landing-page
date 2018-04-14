796b220fc50eff73d1636668c70d9633c0397da7
amberriver93 <cakeinaino@126.com>
2018-04-14 14:31:39 +0800

    Signed-off-by: amberriver93 <cakeinaino@126.com>

---
+++ verification.js
@@ -0,0 +1,16 @@
+/*
+* @Author: Marte
+* @Date:   2018-03-31 20:55:53
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-03-31 23:07:41
+*/
+
+window.onload = function()
+{
+    var showPassword = document.getElementById("checkbox2");
+    var oPassword = document.getElementById("password");
+    showPassword.onclick = function()
+    {
+
+    }
+}
